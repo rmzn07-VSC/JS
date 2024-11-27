f9eababd522fc1dfe259dcbe51eb0ceb1104346e
rmzn07-VSC <ramazanbacaksiz66@gmail.com>
2024-11-27 15:48:49 +0300

    bosluk eklendi

--- kitleIndex.js
+++ kitleIndex.js
@@ -1,31 +1,21 @@
-var kilo = prompt("Lütfen kilonuzu giriniz:");
-var boy = prompt("Lütfen boyunuzu giriniz:");
+var kilo = prompt("Lütfen kilonuzu giriniz: ");
+var boy = prompt("Lütfen boyunuzu giriniz: ");
 
-var boyIndex = boy*boy;
-var kitleIndex = kilo/boyIndex;
+var boyIndex = boy * boy;
+var kitleIndex = kilo / boyIndex;
 
-if(kitleIndex >=45.0){
-    window.alert("Aşırı Obezsiniz... Kitle İndexiniz: " + kitleIndex);
-}
-
-else if (kitleIndex >= 30.0 && kitleIndex <= 44.9) {
+if (kitleIndex >= 45.0) {
+  window.alert("Aşırı Obezsiniz... Kitle İndexiniz: " + kitleIndex);
+} else if (kitleIndex >= 30.0 && kitleIndex <= 44.9) {
   window.alert("Obezsiniz... Kitle İndexiniz: " + kitleIndex);
-}
-
-else if (kitleIndex >= 25.0 && kitleIndex <= 29.9) {
+} else if (kitleIndex >= 25.0 && kitleIndex <= 29.9) {
   window.alert("Fazla kilolusunuz... Kitle İndexiniz: " + kitleIndex);
-}
-
-else if (kitleIndex >= 18.5 && kitleIndex <= 24.9) {
+} else if (kitleIndex >= 18.5 && kitleIndex <= 24.9) {
   window.alert(
     "Normal kilodasınız, gayet sağlıklısınız :) Kitle İndexiniz: " + kitleIndex
   );
-}
-
-else if (kitleIndex >= 8.5 && kitleIndex <= 18.4) {
+} else if (kitleIndex >= 8.5 && kitleIndex <= 18.4) {
   window.alert("Zayıfsınız... Kitle İndexiniz: " + kitleIndex);
-}
-
-else if (kitleIndex <= 8.4) {
+} else if (kitleIndex <= 8.4) {
   window.alert("Aşırı Zayıfsınız... Kitle İndexiniz: " + kitleIndex);
 }
